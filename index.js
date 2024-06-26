16c871e5cf95145c91ec22609b8ac00f6e25e569
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-06-26 11:20:26 +0530

    start

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const port = 9000;
+const path = require('path')
+const users = {} 
+const {Server} = require('socket.io')
+const io = new Server(server)
+app.use(express.static(path.resolve('./public')))
+app.get('/' , (req , res) => {
+    res.sendFile('/public/index.html')
+})
+
+// handle Socket io
+io.on('connection' , (socket) => {
+    socket.on('new-user-join' , name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined' , name) 
+        console.log("New user is : ", name);
+    })
+    socket.on('send' , message => {
+        socket.broadcast.emit('receive' , {message : message , name : users[socket.id]})
+    })
+
+    socket.on('disconnect' , message => {
+        socket.broadcast.emit('leave' , users[socket.id])
+        console.log('disconnect' , users[socket.id]);
+        delete users[socket.id]
+    })
+})
+
+server.listen(port , ()=> {
+    console.log(`Server start at port http://localhost:${port}`);
+})
---
+++ public/script.js
@@ -0,0 +1,45 @@
+const socket = io('http://localhost:9000')
+const messageInput = document.getElementById('messageInp')
+const sendBtn = document.getElementById('sendbtn')
+const allMessage = document.getElementById('allMessages')
+const form = document.getElementById('send-container')
+const centertext = document.querySelector('.center')
+
+// Harry sir
+// New user generated
+
+const appendchild = (message, position) => {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    allMessage.append(messageElement)
+}
+
+const names = prompt("Enter Your name to Join");
+socket.emit('new-user-join', names);
+socket.emit('diconnect')
+
+centertext.innerHTML = `Welcome to the Chat App ${names}`
+
+socket.on('user-joined', (names) => {
+    appendchild(`${names} Joined the chat`, 'center')
+    console.log("New user coming", names);
+})
+
+socket.on('receive', data => {
+    appendchild(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('leave', name => {
+    appendchild(`${name} left the chat`, 'leave')
+})
+
+// Message sending
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    appendchild(`You : ${message}`, 'right')
+    socket.emit('send', message)
+    messageInput.value = "";
+})
