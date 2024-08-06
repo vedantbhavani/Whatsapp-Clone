d5f9a85f54eab42c0ec5d4f17e2415b4536a5787
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-08-06 10:23:42 +0530

    new

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.sendFile('/public/index.html')
 })
 
-// start coding of whtsapp
+// start coding of whatsapp clone
 // handle Socket io
 io.on('connection', (socket) => {
     // New user Joined
